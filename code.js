773ea9834e87c13d120ae97bb34cecdd05f5d44d
jjlippman <jjlippman@earthlink.net>
2016-04-12 14:16:34 -0600

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,23 @@
 'use strict';
+
+function favIceCream(iceCream){
+
+  return "I love " + iceCream
+
+}
+
+function shouting(word){
+
+  return  word.toUpperCase();
+
+}
+
+function roundDown(num){
+  return Math.floor(num);
+ 
+}
+
+function theTruth(){
+   return true;
+ 
+}
